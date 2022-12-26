3e31524aa5df54f8d0059da1d9ce72b9ec9aaf2d
Kyle Johnston <flow14@gmail.com>
2022-12-26 13:37:06 -0600

    Updated RSS implementation…
    Now using Astro's official compiledContent() method
    
    [Docs](https://docs.astro.build/en/guides/rss/#including-full-post-content)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,56 +1,22 @@
-// import rss from '@astrojs/rss';
-
-// const postImportResult = import.meta.glob('../posts/**/*.md', { eager: true });
-// const posts = Object.values(postImportResult);
-
-// export const get = () => rss({
-//   title: 'Buzz’s Blog',
-//   description: 'A humble Astronaut’s guide to the stars',
-//   site: import.meta.env.SITE,
-//   items: posts.map((post) => ({
-//     link: post.url,
-//     title: post.frontmatter.title,
-//     pubDate: post.frontmatter.pubDate,
-//   }))
-// });
-
-
-
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
 
 const postImportResult = import.meta.glob('../**/*.md', { eager: true });
 const posts = Object.values(postImportResult);
 
-async function getPosts() {
-  const unresolved = posts.map(async (post) => {
-    const slug = post.url || post.file.split("/")[post.file.split("/").length - 2];
-
-    return {
-      link: slug,
-      title: post.frontmatter.title,
-      description: post.frontmatter.description,
-      image: post.frontmatter.coverImage,
-      pubDate: post.frontmatter.added,
-      customData: `<content:encoded><![CDATA[${await post.compiledContent()}]]></content:encoded>`,
-  };
-});
-
-return await Promise.all(unresolved);
-
-}
-
 export const get = async () => rss({
   title: 'Kyle Johnston’s Bookshelf',
-  site: 'https://bookshelf.kylewjohnston.com',
-  description: 'A running list of the books I’e read',
-  // SITE will use "site" from your project's astro.config.
-  // list of `<item>`s in output xml
-  // simple example: generate items for every md file in /src/pages
-  // see "Generating items" section for required frontmatter and advanced use cases
-  items: await getPosts(),
+  description: 'A running list of the books I’ve read',
+  site: import.meta.env.SITE,
+  items: posts.map((post) => ({
+    link: post.url,
+    title: post.frontmatter.title,
+    pubDate: post.frontmatter.added,
+    image: post.frontmatter.coverImage,
+    content: sanitizeHtml(post.compiledContent()),
+  })),
+  customData: `<language>en-us</language>`,
   xmlns: {
     content: "http://purl.org/rss/1.0/modules/content/",
   },
-  // (optional) inject custom xml
-  customData: `<language>en-us</language>`,
 });
