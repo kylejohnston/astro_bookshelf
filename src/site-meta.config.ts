3263d3e251b972ffdd1797d912239b8bbf116c57
Kyle Johnston <flow14@gmail.com>
2022-12-26 13:37:33 -0600

    Added SITE environment variable

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,6 +1,7 @@
 const siteMeta = {
   title: "Kyle Johnston’s Bookshelf",
   description: "A running list of the books I’ve read.",
+  site: 'https://bookshelf.kylewjohnston.com',
   lang: "en-US",
   ogLocale: "en_US",
   githubUrl: "https://github.com/kylejohnston/astro_bookshelf",
