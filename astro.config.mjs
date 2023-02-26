018dbc5c83b966df761aecbb7c4d041c984915a4
Kyle Johnston <flow14@gmail.com>
2023-02-26 15:41:33 -0600

    Updated astro.site
    via upgrade to v2: https://docs.astro.build/en/guides/upgrade-to/v2/#changed-astrosite-trailing-slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 // import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({
-  site: 'https://bookshelf.kylewjohnston.com',
+  site: 'https://bookshelf.kylewjohnston.com/',
   integrations: [tailwind()],
 });
