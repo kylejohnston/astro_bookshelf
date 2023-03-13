d5551f74115fd4e44a557d6432c598fb93539c36
Kyle Johnston <flow14@gmail.com>
2023-03-13 15:34:48 -0500

    Update to work with content collections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,23 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
-const postImportResult = import.meta.glob('../**/*.md', { eager: true });
-const posts = Object.values(postImportResult);
+export async function get(context) {
+  const book = await getCollection('book');
+  return rss({
+    title: 'Kyle Johnston’s Bookshelf',
+    description: 'A running list of the books I’ve read',
+    site: context.site,
+    items: book.map((post) => ({
+      link:    `/book/${post.slug}/`,
+      title:   post.data.title,
+      pubDate: post.data.added,
+      image:   post.data.coverImage,
+      // Note: this will not process components or JSX expressions in MDX files.
+      content: sanitizeHtml(parser.render(post.body)),
+    })),
+  });
+}
 
-export const get = async () => rss({
-  title: 'Kyle Johnston’s Bookshelf',
-  description: 'A running list of the books I’ve read',
-  site: import.meta.env.SITE,
-  items: posts.map((post) => ({
-    link: post.url,
-    title: post.frontmatter.title,
-    pubDate: post.frontmatter.added,
-    image: post.frontmatter.coverImage,
-    content: sanitizeHtml(post.compiledContent()),
-  })),
-  customData: `<language>en-us</language>`,
-  xmlns: {
-    content: "http://purl.org/rss/1.0/modules/content/",
-  },
-});
