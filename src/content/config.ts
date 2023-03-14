9b46126e06740cbb03eea0537e78364e9b5dfbcd
Kyle Johnston <flow14@gmail.com>
2023-03-14 11:47:59 -0500

    Cleanup

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,4 @@
 // 1. Import utilities from `astro:content`
-// import { z, defineCollection } from 'astro:content';
-// 2. Define your collection(s)
-
-// defineCollection({
-//   schema: z.object({
-//   })
-// })
-
-// 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 // 2. Define a schema for each collection you'd like to validate.
 const book = defineCollection({
@@ -15,8 +6,8 @@ const book = defineCollection({
     title: z.string(),
     author: z.string(),
     coverImage: z.string(),
-    startDate: z.string().transform((str) => new Date(str)).optional(),
-    finishDate: z.string().transform((str) => new Date(str)),
+    startDate: z.string().optional(),
+    finishDate: z.string().optional(),
     added: z.string().transform((str) => new Date(str)),
     currentlyReading: z.boolean().optional(),
     notes: z.boolean().optional(),
@@ -29,23 +20,3 @@ const book = defineCollection({
 export const collections = {
   'book': book,
 };
-
-
-
-// export const collections = {
-// 	 book: defineCollection({
-// 		schema: z.object({
-// 			title: z.string(),
-//       author: z.string(),
-//       currentlyReading: z.boolean().optional(),
-//       // startDate: z.string().transform((str) => new Date(str)).optional(),
-//       finishDate: z.string().transform((str) => new Date(str)),
-//       added: z.string().transform((str) => new Date(str)),
-//       notes: z.boolean().optional(),
-//       coverImage: z.string(),
-//       library: z.string().optional(),
-//       bookshop: z.string().optional(),
-//       amazon: z.string().optional(),
-// 		}),
-// 	}),
-// };
