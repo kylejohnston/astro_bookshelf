d95b17753123a5e7b7b39b56b75a1be00dbff3e3
Kyle Johnston <flow14@gmail.com>
2023-09-02 16:06:44 -0500

    Upgrade to Astro 3, Tailwind 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-// import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://bookshelf.kylewjohnston.com/',
   integrations: [tailwind()],
-  experimental: {
-    viewTransitions: true
-   }, 
 });
