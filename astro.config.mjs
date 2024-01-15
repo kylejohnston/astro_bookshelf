89acee0e24473405ebe833c9fe72e7d57ced7bfb
Kyle Johnston <flow14@gmail.com>
2024-01-15 11:41:21 -0600

    Added astro-pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import pagefind from 'astro-pagefind';
+
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   site: 'https://bookshelf.kylewjohnston.com/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), pagefind()],
 });
