dd7bb68e289a4784f8a655648094bbe24ea4fa38
Kyle Johnston <flow14@gmail.com>
2025-08-02 15:48:00 -0500

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Kyle Johnston\’s Bookshelf';
+export const SITE_DESCRIPTION = 'A running list of the books I\’ve read';
+export const SITE_URL = 'https://books.kyle.io';
